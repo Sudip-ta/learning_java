39f9dac5089d11f3870c7e453ecbf506b8b956fd
Sudipta <sudiptabhatta34@gmail.com>
2026-06-27 14:10:44 +0530

    method_overloading

--- Method_Overloading/Main.java
+++ Method_Overloading/Main.java
@@ -1,3 +1,24 @@
+class Calculator{
+    public int add(int a,int b){
+        return (a+b);
+    }
+    public int add(int a, int b, int c){
+        return (a+b+c);
+    }
+    public double add(int a, double b){
+        return a+b;
+    }
+}
+
 public class Main{
-    
+    public static void main(String[]args){
+        Calculator calc = new Calculator();
+        int r1 = calc.add(1,2);
+        System.out.println(r1);
+        int r2 = calc.add(1,2,3);
+        System.out.println(r2);
+        double r3 = calc.add(1,2.0);
+        System.out.println(r3);
+
+    }
 }
