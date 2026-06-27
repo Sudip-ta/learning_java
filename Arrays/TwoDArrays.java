8e37a60baee53abbe7a364b97239cbc45597dfc4
Sudipta <sudiptabhatta34@gmail.com>
2026-06-27 17:29:39 +0530

    2D arrays

---
+++ Arrays/TwoDArrays.java
@@ -0,0 +1,20 @@
+package Arrays;
+public class TwoDArrays{
+    public static void main(String[]args){
+        // int arr1[][] = {{1,2,3},{4,5,6}};
+        int nums[][] = new int [3][3];
+
+        for(int i = 0;i<nums.length;i++){
+            for(int j = 0;j<nums[i].length;j++){
+                nums[i][j] = (int)(Math.random()*10);
+            }
+        }
+
+        for(int i = 0;i<nums.length;i++){
+            for(int j = 0;j<nums[i].length;j++){
+                System.out.print(nums[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
