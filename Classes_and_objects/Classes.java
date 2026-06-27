bfd55cb51a212b9f980f6f72c5dda60d80a0e571
Sudipta <sudiptabhatta34@gmail.com>
2026-06-27 13:52:45 +0530

    classes

---
+++ Classes_and_objects/Classes.java
@@ -0,0 +1,18 @@
+class Calculator{
+    int a;
+    int b;
+    Calculator(int a, int b){  //Class Constructor
+        this.a = a;
+        this.b = b;
+    }
+    public void add(){
+        System.out.println("Sum : "+(a+b));
+    }
+}
+
+public class Classes{
+    public static void main(String[]args){
+        Calculator c = new Calculator(2,3); 
+        c.add();
+    }
+}
