12657b15bc611f3def4fc830eee5f58536e8a5dd
Sudipta <sudiptabhatta34@gmail.com>
2026-06-29 18:51:39 +0530

    erto correction

--- ThisAndSuper/ThisAndSuper.java
+++ ThisAndSuper/ThisAndSuper.java
@@ -1,7 +1,7 @@
 package ThisAndSuper;
 class A{
     public A() {
-        super();  //Every constructor by default has a super method in the first line of it's code
+        this(5);
         System.out.println("in A");
     }
     public A(int n){
@@ -12,20 +12,22 @@ class A{
 
 class B extends A{
     public B() {
-        super();
+        super(); //Calls the default constructor of A(super class)
         System.out.println("in B");
     }
     public B(int n){
         this(); //calling this() which is a constructor of the same class, so it calls the default constructor of the same class
-        super(n); //passing super which a parameter so it calls the parameterised constructor of super class
-        super();//Calls the default constructor of A(super class)
         System.out.println("in B with int");
     }
 }
 
-public class SuperMethod {
+public class ThisAndSuper {
     public static void main(String[] args) {
-        B b1 = new B(); //in A in B
-        B b2 = new B(20);// in B in A with int in B with int in A
+        B b2 = new B(20);
     }
 }
+// Output
+// in A with int
+// in A
+// in B
+// in B with int
